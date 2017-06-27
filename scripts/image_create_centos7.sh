f68c7bc83decd44d2d830f3a045f88534ef08cee
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2017-06-27 10:03:48 +0300

    Remove minor version from EL variants
    Remove minor version from EL variants. Remove Ubuntu 15.10 from
    crontab. Add v3 openrcs.

--- scripts/image_create_centos7.sh
+++ scripts/image_create_centos7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash --login
-IMAGE_NAME="CentOS-7.0"
+IMAGE_NAME="CentOS-7"
 CLOUD_INIT_DEFAULT_USER_NAME="cloud-user"
 ELEMENTS="vm cloud-init-cfg centos7"
 PACKAGES="vim,ntp,deltarpm"
--- scripts/image_create_scientific7.sh
+++ scripts/image_create_scientific7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash --login
-IMAGE_NAME="ScientificLinux-7.0"
+IMAGE_NAME="ScientificLinux-7"
 CLOUD_INIT_DEFAULT_USER_NAME="cloud-user"
 ELEMENTS="vm cloud-init-cfg scientific7"
 PACKAGES="vim,ntp,deltarpm"
