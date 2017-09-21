00bae44d8aa88aaea8dbfff9e0228c855fb09e1e
Darren Glynn <darren.glynn@csc.fi>
2017-09-21 13:02:30 +0300

    Make our Cuda images public
    Testing is now done and we are releasing the GPGPU nodes 22.9.2107. For
    this we now make the scripts build the images and make them public by
    default.

--- scripts/image_create_centos7-cuda.sh
+++ scripts/image_create_centos7-cuda.sh
@@ -4,7 +4,7 @@ IMAGE_NAME="CentOS-7-Cuda"
 CLOUD_INIT_DEFAULT_USER_NAME="cloud-user"
 ELEMENTS="vm cloud-init-cfg centos7 nvidia-cuda"
 PACKAGES="vim,ntp,deltarpm,cuda"
-IMAGE_VISIBILITY="private"
+IMAGE_VISIBILITY="public"
 
 export CLOUD_INIT_DEFAULT_USER_NAME
 
