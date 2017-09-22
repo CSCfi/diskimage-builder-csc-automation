e750b8ba13c2f464685964874a04dc59aeb2680b
Darren Glynn <dglynn@users.noreply.github.com>
2017-09-22 14:10:28 +0300

    Make our Cuda images public (#18)
    Testing is now done and we are releasing the GPGPU nodes 22.9.2107. For
    this we now make the scripts build the images and make them public by
    default.

--- scripts/image_create_ubuntu-16.04-cuda.sh
+++ scripts/image_create_ubuntu-16.04-cuda.sh
@@ -4,7 +4,7 @@ IMAGE_NAME="Ubuntu-16.04-Cuda"
 CLOUD_INIT_DEFAULT_USER_NAME="cloud-user"
 ELEMENTS="vm cloud-init-cfg ubuntu nvidia-cuda"
 PACKAGES="vim,ntp,python,linux-headers-generic,cuda"
-IMAGE_VISIBILITY="private"
+IMAGE_VISIBILITY="public"
 
 export DIB_RELEASE
 export CLOUD_INIT_DEFAULT_USER_NAME
