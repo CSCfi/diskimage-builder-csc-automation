9b9b2861fc8c86cc2f9ba6201e6a63a4b696fa22
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2017-09-28 10:18:39 +0300

    Configurable host URL for download
    Use configurable variable containing protocol://host/path to be
    used for image download.

--- scripts/image_create_fedora_atomic_dl.sh
+++ scripts/image_create_fedora_atomic_dl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash --login
-REL_VERSION=$(curl -Ls https://download.fedoraproject.org/pub/alt/atomic/stable/ |grep -Eo 'Fedora-Atomic-25-[0-9\.]*'|sort -r|head -1)
-DOWNLOAD_URL="https://download.fedoraproject.org/pub/alt/atomic/stable/$REL_VERSION/CloudImages/x86_64/images/$REL_VERSION.x86_64.qcow2"
+ATOMIC_URL="https://download.fedoraproject.org/pub/alt/atomic/stable"
+REL_VERSION=$(curl -Ls $ATOMIC_URL |grep -Eo 'Fedora-Atomic-25-[0-9\.]*'|sort -r|head -1)
+DOWNLOAD_URL="$ATOMIC_URL/$REL_VERSION/CloudImages/x86_64/images/$REL_VERSION.x86_64.qcow2"
 IMAGE_NAME="Fedora-Atomic-25"
 
 source $(dirname $0)/image_dl_test_deploy.sh "$DOWNLOAD_URL"
