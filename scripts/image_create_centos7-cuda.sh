a681d66b54c45ddc2dba3cefdd1a71dab866cd09
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2017-10-25 18:32:52 +0300

    Add dummy file fron cron
    Add a dummy file to act as target for cloud-init-cfg cronjob.

--- scripts/image_create_centos7-cuda.sh
+++ scripts/image_create_centos7-cuda.sh
@@ -5,8 +5,10 @@ CLOUD_INIT_DEFAULT_USER_NAME="cloud-user"
 ELEMENTS="vm cloud-init-cfg centos7 nvidia-cuda"
 PACKAGES="vim,ntp,deltarpm,cuda"
 IMAGE_VISIBILITY="public"
+CLOUD_INIT_CRON_UPDATES_FILE="/tmp/.nope"
 
 export CLOUD_INIT_DEFAULT_USER_NAME
+export CLOUD_INIT_CRON_UPDATES_FILE
 
 source $(dirname $0)/image_create.sh
 
