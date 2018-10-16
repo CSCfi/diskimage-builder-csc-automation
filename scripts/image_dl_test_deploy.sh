aa309afe3a166bbf3d8f8941b1c9c48c4edf5316
Johan Guldmyr <johan.guldmyr@csc.fi>
2018-10-16 09:11:52 +0300

    Add Ubuntu 18.04 images straight from upstream
     - #CCCP-1853 some prestudy was done - making our diskimage-builder
       build 18.04 was not easy
     - Using the method in this was easy:
      - download the latest bionic cloud image (they are built daily by
        canonical)
      - run our usual stuff (upload temp image, spawn an instance, etc)
     - The upstream Ubuntu cloud-image has cloud-init installed
      - Default user name is ubuntu, not cloud-user

--- scripts/image_dl_test_deploy.sh
+++ scripts/image_dl_test_deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # Download cloud image, push it to glance and create test instance
 
+if [[ $(dirname $0) == *epouta* ]]; then
+source $(dirname $0)/image_epouta_constants.sh
+else
 source $(dirname $0)/image_cpouta_constants.sh
+fi
 source $(dirname $0)/image_functions.sh
 
 set -eu
