c4effbb73849ce51cf966b7cb3fb863b722f86ed
Matteo Pozza <matteo.pozza@csc.fi>
2021-04-15 15:14:30 +0300

    Change image format from qcow2 to raw
    The commit not only switches the format from qcow2 to raw, but makes it
    so that it should be easy to switch between the two formats by just
    changing the image format in the constants file.

--- scripts/image_functions.sh
+++ scripts/image_functions.sh
@@ -6,6 +6,8 @@ function cleanup() {
     find . -maxdepth 5 -name "${IMAGE_NAME}*.d" -type d -exec rm -rf {} +
     echo "Deleting ${IMAGE_NAME}*.qcow2 files"
     find . -maxdepth 5 -name "${IMAGE_NAME}*.qcow2" -type f -exec rm -f {} +
+    echo "Deleting ${IMAGE_NAME}*.raw files"
+    find . -maxdepth 5 -name "${IMAGE_NAME}*.raw" -type f -exec rm -f {} +
     # delete any orphaned temporary images using image_id
     echo "Deleting old images from OS"
     for id in $(glance image-list | grep "$TMP_IMAGE_NAME" | awk '{print $2}')
@@ -23,11 +25,14 @@ function cleanup() {
 function image_create() {
 	# create cloud image with diskimage-builder
 	TMP_DIR=~/temp disk-image-create $DIB_OPTIONS -o "$IMAGE_NAME" \
-	    --image-size "$IMAGE_SIZE" -p "$PACKAGES" $ELEMENTS
+	    --image-size "$IMAGE_SIZE" -p "$PACKAGES" -t "$IMAGE_FORMAT" $ELEMENTS
 }
 
 function image_download() {
     curl -L -o ${IMAGE_NAME}.qcow2 "$1"
+    if [ "$IMAGE_FORMAT" != "qcow2" ]; then
+        qemu-img convert -f qcow2 -O ${IMAGE_FORMAT} ${IMAGE_NAME}.qcow2 ${IMAGE_NAME}.${IMAGE_FORMAT}
+    fi
 }
 
 function image_test() {
