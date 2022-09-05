3659f531928f045f16b518a7e62ea13db9301f6c
Matteo Pozza <matteo.pozza@csc.fi>
2022-09-05 19:01:28 +0300

    Cleanup the repository
    - removes the file for creating CentOS-8 (non-stream version)
    - tunes the image building process, so that the description stating
    "all packages are updated" is only applies with CentOS-7 images

--- scripts/image_create_centos8.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash --login
-DISTRO_URL="https://cloud.centos.org/centos/8/x86_64/images/"
-DOWNLOAD_URL="$DISTRO_URL/CentOS-8-GenericCloud-8.1.1911-20200113.3.x86_64.qcow2"
-IMAGE_NAME="CentOS-8"
-OS_DISTRO_PROPERTY="centos"
-IMAGE_VISIBILITY="public"
-
-source $(dirname $0)/image_dl_test_deploy.sh "$DOWNLOAD_URL"
--- scripts/image_functions.sh
+++ scripts/image_functions.sh
@@ -73,9 +73,13 @@ function image_deploy() {
     fi
     # rename new image
     echo "Renaming $TMP_IMAGE_NAME to $IMAGE_NAME"
+    DESCRIPTION=""
+    if [[ "$IMAGE_NAME" == "CentOS-7"* ]]; then
+      DESCRIPTION="All packages of this image were updated on $(date +%F). To find out which user to login with: ssh in as root."
+    fi
     glance image-update --name "$IMAGE_NAME" \
-        --property description="All packages of this image were updated on $(date +%F). To find out which user to login with: ssh in as root." \
-        --visibility "$IMAGE_VISIBILITY" \
+      --property description="$DESCRIPTION" \  
+      --visibility "$IMAGE_VISIBILITY" \
         $(glance image-list | grep "$TMP_IMAGE_NAME" | cut -d '|' -f2)
 }
 
