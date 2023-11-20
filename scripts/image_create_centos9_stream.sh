02fd945a16cd07e0cfbe276eabe62712376b189e
Matteo Pozza <matteo.pozza@csc.fi>
2023-11-20 18:23:41 +0200

    Simplify centos stream image fetching

--- scripts/image_create_centos9_stream.sh
+++ scripts/image_create_centos9_stream.sh
@@ -1,7 +1,6 @@
 #!/bin/bash --login
-DISTRO_URL="https://cloud.centos.org/centos/9-stream/x86_64/images/"
-DISTRO_NAME=$(curl -s "$DISTRO_URL?C=M;O=D" | grep -oE "*CentOS-Stream-GenericCloud-9-[0-9]{8}.[0-9]{1}.x86_64.qcow2*" | head -n 1)
-DOWNLOAD_URL="$DISTRO_URL$DISTRO_NAME"
+DISTRO_URL="https://cloud.centos.org/centos/9-stream/x86_64/images"
+DOWNLOAD_URL="$DISTRO_URL/CentOS-Stream-GenericCloud-9-latest.x86_64.qcow2"
 IMAGE_NAME="CentOS-9-Stream"
 OS_DISTRO_PROPERTY="centos"
 IMAGE_VISIBILITY="public"
